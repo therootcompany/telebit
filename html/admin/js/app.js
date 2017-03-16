72b4c4598f60a9c594f15de9d6387a2c841e1cfb
Henry Camacho <henry.f.camacho@gmail.com>
2017-03-15 19:32:05 -0500

    Admin is functioning — Displays Servers that are connected with in/out bytes.

--- html/admin/js/app.js
+++ html/admin/js/app.js
@@ -1,13 +1,15 @@
 console.log("app.sh startup")
 
-
 var app = angular.module("rvpnApp", ["ngRoute"]);
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
-    .when("/admin/servers", {
+    .when("/admin/index.html", {
         templateUrl : "admin/partials/servers.html"
     })
-    .when("/admin/domains", {
+    .when("/admin/servers/", {
+        templateUrl : "admin/partials/servers.html"
+    })
+    .when("/admin/#domains", {
         templateUrl : "green.htm"
     })
     .when("/blue", {
@@ -15,3 +17,28 @@ app.config(function($routeProvider, $locationProvider) {
     });
     $locationProvider.html5Mode(true);
 });
+
+app.controller('serverController', function ($scope, $http) {
+    $scope.servers = [];
+    var api = '/api/com.daplie.rvpn/servers'
+
+    $http.get(api).then(function(response) {
+        updateView(response.data);
+    });
+
+    updateView = function(data) {
+        console.log(data);
+        if (data.error == 'ok' ){
+            console.log("ok")
+            $scope.servers = data.result.servers;
+            console.log(data.result)
+
+        }
+    };
+
+
+
+
+
+});
+    
