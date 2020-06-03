077ea2d12de4d0f71919ff305075719b0ca6af83
AJ ONeal <coolaj86@gmail.com>
2020-06-03 00:23:50 -0600

    add build scripts for convenience

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ set -u
 #CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -mod vendor -o telebit-relay-windows.exe ./cmd/telebit-relay/telebit-relay.go
 
 go generate ./...
+
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -mod vendor -o telebit-client-linux ./cmd/telebit/*.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -mod vendor -o telebit-client-macos ./cmd/telebit/*.go
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -mod vendor -o telebit-client-windows-debug.exe ./cmd/telebit/*.go
