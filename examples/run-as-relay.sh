c951ce12548b99d5cc648168c65371543037f4e9
AJ ONeal <coolaj86@gmail.com>
2020-07-06 09:59:05 +0000

    update ENVs

--- examples/run-as-relay.sh
+++ examples/run-as-relay.sh
@@ -3,17 +3,20 @@
 set -e
 set -u
 
-go generate -mod=vendor ./...
+#go generate -mod=vendor ./...
 go build -mod=vendor -o telebit cmd/telebit/*.go
 
 source .env
 
 ADMIN_HOSTNAME=${ADMIN_HOSTNAME:-"devices.example.com"}
 AUTH_BASEURL=${AUTH_BASEURL:-"https://devices.example.com"}
+AUTH_URL=${AUTH_URL:-"$AUTH_BASEURL/api"}
 SECRET=${SECRET:-"xxxxxxxxxxxxxxxx"}
+ACME_EMAIL="${ACME_EMAIL:-}"
 
 ./telebit --acme-agree=true \
     --admin-hostname $ADMIN_HOSTNAME \
-    --auth-url $AUTH_BASEURL/api \
+    --auth-url $AUTH_URL/api \
+    --acme-email "$ACME_EMAIL" \
     --secret "$SECRET" \
     --listen 3020,3030
