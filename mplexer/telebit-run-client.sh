cfe3820feadd14e787cf0c57ac9d9f35a2de4e24
AJ ONeal <coolaj86@gmail.com>
2020-07-20 09:31:28 -0600

    remove old scripts

--- mplexer/telebit-run-client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-go run cmd/telebit/*.go --acme-agree=true \
-    --acme-relay http://devices.rootprojects.org:3010/api/dns \
-    --auth-url http://devices.rootprojects.org:3010/api \
-    --relay wss://devices.rootprojects.org:8443/api/ws \
-    --app-id test-id \
-    --secret k7nsLSwNKbOeBhDFpbhwGHv \
-    --listen 3443
